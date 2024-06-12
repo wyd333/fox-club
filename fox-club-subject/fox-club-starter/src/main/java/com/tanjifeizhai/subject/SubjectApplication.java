db2272d3447eb7b79cce34aac93980398df94254
bit-yidan <1256970054@qq.com>
2024-06-12 11:54:46 +0800

    SpringBoot集成;设置starter启动类

---
+++ fox-club-subject/fox-club-starter/src/main/java/com/tanjifeizhai/subject/SubjectApplication.java
@@ -0,0 +1,21 @@
+package com.tanjifeizhai.subject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description: 刷题微服务启动类
+ * User: 12569
+ * Date: 2024-06-12
+ * Time: 11:37
+ */
+
+@SpringBootApplication
+@ComponentScan("com.tanjifeizhai")
+public class SubjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubjectApplication.class);
+    }
+}
